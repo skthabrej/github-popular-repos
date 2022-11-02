de63bbd2896952dfe73e70aecba1898f97cff353
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-11-02 11:20:46 +0530

    github-popular-repos

--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -7,16 +7,16 @@ const RepositoryItem = props => {
             <img className='card-item-image' src={repositoryData.imageUrl} alt={repositoryData.name}/>
             <h1 className='card-item-name'>{repositoryData.name}</h1>
             <div className='stats-container'>
-                <img src="" alt='' className='stats-icon'/>
-                <p className='stats-text' >stars</p>
+                <img src="https://i.postimg.cc/hjjMdkQR/icons-ge6846ec9b-1280.png" alt='star' className='stats-icon'/>
+                <p className='stats-text'>{repositoryData.starsCount} Stars</p>
             </div>
             <div className='stats-container'>
-                <img src="" alt='' className='stats-icon'/>
-                <p className='stats-text' >stars</p>
+                <img src="https://i.postimg.cc/XvJ4rvLj/ip-access-control-1.png" alt='fork' className='stats-icon'/>
+                <p className='stats-text' >{repositoryData.forksCount} Forks</p>
             </div>
             <div className='stats-container'>
-                <img src="" alt='' className='stats-icon'/>
-                <p className='stats-text' >stars</p>
+                <img src="https://i.postimg.cc/JnLHwZm6/warning.png" alt='issue' className='stats-icon'/>
+                <p className='stats-text' >{repositoryData.issuesCount} open issues</p>
             </div>
         </li>
     )
